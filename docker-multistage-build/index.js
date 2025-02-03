fe6b7b1b32128d1be4f25e1ddeb167c1a325caac
anjali <abc@gmail.com>
2025-02-03 12:52:11 +0000

    commit1

---
+++ docker-multistage-build/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Define a route for the app
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Set the port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
